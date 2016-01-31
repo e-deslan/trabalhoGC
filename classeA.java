7b82fe8c7510c109f6908f404afcadb72b210ced
Eduardo Deslandes <eduardo.deslandes@gmail.com>
2016-01-31 20:28:32 -0200

    criado atributo Aramo4

--- classeA.java
+++ classeA.java
@@ -3,6 +3,7 @@ public class A{
 	private int A1;
 	private float A2;
 	private String A3;
+	private String Aramo4;
 
 	public int getA1(){
 		return A1;
@@ -26,6 +27,14 @@ public class A{
 
 	public void setA3(String A3){
 		this.A3 = A3;
+	}
+
+	public String getAramo4(){
+		return Aramo4;
+	}
+
+	public void setAramo4(String Aramo4){
+		this.Aramo4 = Aramo4;
 	}
 
 	public float soma(){
