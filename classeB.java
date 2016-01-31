2d53025eeec043ca0e223d99f835ffd9eebdec91
Eduardo Deslandes <eduardo.deslandes@gmail.com>
2016-01-31 20:50:03 -0200

    criada classeB

---
+++ classeB.java
@@ -0,0 +1,22 @@
+public class B{
+	
+	private int B1;
+	private float B2;
+
+	public int getB1(){
+		return B1;
+	}
+
+	public void setB1(int B1){
+		this.B1 = B1;
+	}
+
+	public float getB2(){
+		return B2;
+	}
+
+	public void setB2(float B2){
+		this.B2 = B2;
+	}
+
+}
